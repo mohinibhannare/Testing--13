b4fed57450a72590a56ea1be831f4deb5de69d8f
mohinibhannare <99393763+mohinibhannare@users.noreply.github.com>
2022-02-11 13:15:12 +0530

    Update DEMO1.java

--- Git PROJECT22/src/DEMO1.java
+++ Git PROJECT22/src/DEMO1.java
@@ -15,6 +15,13 @@ public class DEMO1
 		System.out.println("This is m2 method of DEMO1 class ");
 	}
 	
+	
+	public void m3() 
+	{
+		System.out.println("This is m3 method of DEMO1 class ");
+	}
+	
+	
 		public static void main(String[] args) {
 		
 		
